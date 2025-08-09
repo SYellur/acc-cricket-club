15ae86f0bec0155e4fd72ac0867e6bb1000faef4
Suhas Yellur <suhas.yellur@hotmail.com>
2025-08-09 10:24:52 +0100

    add Vercel analytics and speed insights; update layout to include analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css';
+// import '../styles/globals.css';
 import type { ReactNode } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 export const metadata = {
   title: 'Adamstown Cricket Club',
   description: 'Adamstown Cricket Club — wine & grey, glossy site',
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <Header />
         <main className="min-h-screen">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
